cd5f9833005650a232a2209658d4f2f612499c9d
John Sumsion <jdsumsion@gmail.com>
2011-12-13 08:19:32 -0700

    Hacked together a brand-new commit chain on an as-of-yet-non-existant branch.

--- experiments/grit_play.rb
+++ experiments/grit_play.rb
@@ -3,8 +3,12 @@ require 'pp'
 
 FileUtils.rm_rf("test.git")
 repo = Grit::Repo.init_bare_or_open("test.git")
+tree = repo.git.write_tree #empty
+repo.index.commit("first commit", { :commit_tree_sha => tree, :head => "some-branch" })
+commits = repo.commits("some-branch")
+pp commits
+
 index = repo.index
 index.add("hello/world/file", "contents")
-pp index
-repo.commit_index("my commit")
-pp repo.commits
+index.commit("my commit", { :head => "some-branch", :parents => [commits.first] })
+pp repo.commits("some-branch")
